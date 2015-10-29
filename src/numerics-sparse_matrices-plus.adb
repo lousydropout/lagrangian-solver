fb2ed61454522e04a9f85a437d5eab9c7e61cb82
Vincent Chan <lousydropout@gmail.com>
2015-10-30 05:21:45 +0900

    simplified sparse matrix addition routine

--- src/numerics-sparse_matrices-plus.adb
+++ src/numerics-sparse_matrices-plus.adb
@@ -2,91 +2,66 @@ separate (Numerics.Sparse_Matrices)
 
 function Plus (Left  : in Sparse_Matrix;
 	       Right : in Sparse_Matrix) return Sparse_Matrix is
+   type Bool_Array is array (Nat range <>) of Boolean with
+     Pack => True;
    N_Col  : Nat renames Left.N_Col;
    N_Row  : Nat renames Left.N_Row;
    Result : Sparse_Matrix;
    Sum    : Pos;
    Value  : Real_Array (1 .. N_Row);
-   Exist  : Int_Array  (1 .. N_Row);
+   Exist  : Bool_Array (1 .. N_Row);
    P      : Int_Array  (1 .. N_Col + 1) := (others => 0);
-   
-   L1, L2, R1, R2 : Nat;
+
    use Ada.Containers;
 begin
    
    Result.Format := CSC;
    Result.N_Col  := N_Col;
    Result.N_Row  := N_Row;
-   Result.P.Set_Length (Count_Type (N_Col + 1));
    
    Sum := 0;
    for I in 1 .. Nat (N_Col) loop
-      Exist := (others => 0);
+      Exist := (others => False);
       
-      if I < Nat (Left.P.Length) then
-	 L1 := Left.P (I); 
-	 L2 := Left.P (I + 1) - 1;
-	 if L1 <= L2 then
-	    for J in L1 .. L2 loop
-	       Exist (Left.I (J)) := 1;
-	    end loop;
-	 end if;
-      end if;
+      for J in Left.P (I) .. Left.P (I + 1) - 1 loop
+	 Exist (Left.I (J)) := True;
+      end loop;
       
-      if I < Nat (Right.P.Length) then
-	 R1 := Right.P (I); 
-	 R2 := Right.P (I + 1) - 1;
-	 if R1 <= R2 then 
-	    for J in R1 .. R2 loop
-	       Exist (Right.I (J)) := 1;
-	    end loop;
-	 end if;
-      end if;
+      for J in Right.P (I) .. Right.P (I + 1) - 1 loop
+	 Exist (Right.I (J)) := True;
+      end loop;
       
       -- Sum over Exist
       for E of Exist loop
-	 P (I) := P (I) + E;
+	 P (I) := P (I) + 1;
       end loop;
       Sum := Sum + P (I);
    end loop;
    P := Cumulative_Sum (P);
    
    Set (Result.P, P);
-   Result.X.Set_Length (Count_Type (Sum));
-   Result.I.Set_Length (Count_Type (Sum));
+   Result.X.Reserve_Capacity (Count_Type (Sum));
+   Result.I.Reserve_Capacity (Count_Type (Sum));
    
    
-   Sum := 0;      
    for I in 1 .. Nat (N_Col) loop
       Value := (others => 0.0);
-      Exist := (others => 0);
-      if I < Nat (Left.P.Length) then
-	 L1 := Left.P (I); 
-	 L2 := Left.P (I + 1) - 1;
-	 if L1 <= L2 then
-	    for J in L1 .. L2 loop
-	       Exist (Left.I (J)) := 1;
-	       Value (Left.I (J)) := Left.X (J);
-	    end loop;
-	 end if;
-      end if;
-      
-      if I < Nat (Right.P.Length) then
-	 R1 := Right.P (I); 
-	 R2 := Right.P (I + 1) - 1;
-	 if R1 <= R2 then 
-	    for J in R1 .. R2 loop
-	       Exist (Right.I (J)) := 1;
-	       Value (Right.I (J)) := Value (Right.I (J)) + Right.X (J);
-	    end loop;
-	 end if;
-      end if;
+      Exist := (others => False);
+
+      for J in Left.P (I) .. Left.P (I + 1) - 1 loop
+	 Exist (Left.I (J)) := True;
+	 Value (Left.I (J)) := Left.X (J);
+      end loop;
+   
+      for J in Right.P (I) .. Right.P (I + 1) - 1 loop
+	 Exist (Right.I (J)) := True;
+	 Value (Right.I (J)) := Value (Right.I (J)) + Right.X (J);
+      end loop;
       
       for K in Value'Range loop
-	 if Exist (K) = 1 then
-	    Sum := Sum + 1;
-	    Result.I (Sum) := K;
-	    Result.X (Sum) := Value (K);
+	 if Exist (K) then
+	    Result.I.Append (K);
+	    Result.X.Append (Value (K));
 	 end if;
       end loop;
    end loop;
