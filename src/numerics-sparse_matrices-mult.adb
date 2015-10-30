d2760610f61f078480359e07e2230c239e352cb4
Vincent Chan <lousydropout@gmail.com>
2015-10-31 01:42:41 +0900

    added reserve capacity statement in function mult

--- src/numerics-sparse_matrices-mult.adb
+++ src/numerics-sparse_matrices-mult.adb
@@ -1,13 +1,14 @@
 separate (Numerics.Sparse_Matrices)
 
 function Mult (Left, Right : in Sparse_Matrix) return Sparse_Matrix is
-   use Ada.Containers, Ada.Text_IO;
+   use Ada.Containers;
    A  : Sparse_Matrix renames Left;
    B  : Sparse_Matrix renames Right;
    C  : Sparse_Matrix;
    X  : Real_Array (1 .. A.N_Row) := (others => 0.0);
-   W  : Int_Array (1 .. A.N_Row)  := (others => 0);
+   W  : Int_Array  (1 .. A.N_Row) := (others => 0);
    Nz : Pos := 1;
+   N_Row : constant Count_Type := Count_Type (A.N_Row) + 1;
    
    procedure Scatter (A	   : in     Sparse_Matrix;
 		      J	   : in     Int;
@@ -31,12 +32,16 @@ function Mult (Left, Right : in Sparse_Matrix) return Sparse_Matrix is
 	 end if;
       end loop;
    end Scatter;
-   
+
 begin
    C.Format := CSC; C.N_Row := A.N_Row; C.N_Col := B.N_Col;
-   C.P.Reserve_Capacity (Count_Type (B.N_Col) + 1);
+   C.P.Reserve_Capacity (N_Row);
    
    for J in 1 .. B.N_Col loop
+      if C.I.Capacity < C.I.Length + N_Row then
+      	 C.I.Reserve_Capacity (C.I.Capacity + N_Row);
+      end if;
+      
       C.P.Append (Nz);
       for K in B.P (J) .. B.P (J + 1) - 1 loop
 	 Scatter (A, B.I (K), B.X (K), W, X, J, C, Nz);
@@ -48,7 +53,13 @@ begin
    end loop;
    C.P.Append (Nz);
    
-   C.Convert; 
-   C.Convert;
+   
+   -- Remove extra space hogged by sparse matrix C
+   C.I.Reserve_Capacity (C.I.Length);
+   C.X.Reserve_Capacity (C.I.Length);
+   
+   -- Need to sort entries
+   C.Convert; C.Convert;
+   
    return C;
 end Mult;
