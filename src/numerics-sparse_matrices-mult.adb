8c46e20c751c0b9c8cead42f942120c1b78f1faf
Vincent Chan <lousydropout@gmail.com>
2015-10-31 02:05:47 +0900

    for mult: began by reserving 1% of a dense product

--- src/numerics-sparse_matrices-mult.adb
+++ src/numerics-sparse_matrices-mult.adb
@@ -9,6 +9,7 @@ function Mult (Left, Right : in Sparse_Matrix) return Sparse_Matrix is
    W  : Int_Array  (1 .. A.N_Row) := (others => 0);
    Nz : Pos := 1;
    N_Row : constant Count_Type := Count_Type (A.N_Row) + 1;
+   N_Res : constant Count_Type := Count_Type (A.N_Row * B.N_Col / 100);
    
    procedure Scatter (A	   : in     Sparse_Matrix;
 		      J	   : in     Int;
@@ -23,10 +24,10 @@ function Mult (Left, Right : in Sparse_Matrix) return Sparse_Matrix is
       for P in A.P (J) .. A.P (J + 1) - 1 loop
 	 I := A.I (P);
 	 if W (I) < Mark then
-	    W (I) := Mark;
 	    C.I.Append (I);
-	    Nz := Nz + 1;
+	    W (I) := Mark;
 	    X (I) := β * A.X (P);
+	    Nz    := Nz + 1;
 	 else
 	    X (I) := X (I) + β * A.X (P);
 	 end if;
@@ -36,10 +37,13 @@ function Mult (Left, Right : in Sparse_Matrix) return Sparse_Matrix is
 begin
    C.Format := CSC; C.N_Row := A.N_Row; C.N_Col := B.N_Col;
    C.P.Reserve_Capacity (N_Row);
+   C.I.Reserve_Capacity (N_Res);
+   C.X.Reserve_Capacity (N_Res);
    
    for J in 1 .. B.N_Col loop
       if C.I.Capacity < C.I.Length + N_Row then
       	 C.I.Reserve_Capacity (C.I.Capacity + N_Row);
+      	 C.X.Reserve_Capacity (C.X.Capacity + N_Row);
       end if;
       
       C.P.Append (Nz);
@@ -53,10 +57,9 @@ begin
    end loop;
    C.P.Append (Nz);
    
-   
    -- Remove extra space hogged by sparse matrix C
    C.I.Reserve_Capacity (C.I.Length);
-   C.X.Reserve_Capacity (C.I.Length);
+   C.X.Reserve_Capacity (C.X.Length);
    
    -- Need to sort entries
    C.Convert; C.Convert;
