68b89d7cdf436dc6091c151093a58340e5a585dd
Vincent Chan <lousydropout@gmail.com>
2015-11-01 08:56:32 +0900

    corrected kronecker

--- src/sparse_test.adb
+++ src/sparse_test.adb
@@ -5,18 +5,17 @@ with Ada.Text_IO; use Ada.Text_IO;
 procedure Sparse_Test is
    N : Int := 2;
    
-   I1 : Int_Array  := (2,   3,   1,  2);
+   I1 : Int_Array  := (2,   1,   1,  2);
    J1 : Int_Array  := (1,   3,   1,  3);
-   X1 : Real_Array := (1.234, 2.345, 2.789, 1.2);
+   X1 : Real_Array := (1.0, 2.0, 3.0, 5.0);
    
    A  : Sparse_Matrix := Triplet_To_Matrix (I1, J1, X1);
    B  : Sparse_Matrix := Triplet_To_Matrix (J1, I1, X1);
    
    C : Sparse_Matrix;
 begin
-   C := A + B;
+   C := B and A;
    --  A := Transpose (A) + Transpose (B);
-   A.Transposed;
    A.Print;
    --  B.Transposed;
    B.Print;
